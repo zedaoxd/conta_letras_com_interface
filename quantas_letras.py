3c0502d6b0c10c1823d2463013a3916f39084815
Bruno Lessa <Brunolessa@live.com>
2021-07-03 23:41:38 -0300

    up

---
+++ quantas_letras.py
@@ -0,0 +1,37 @@
+from tkinter import *
+from tkinter import messagebox
+
+
+def conta_string(texto):
+    ja_foi = []
+    contadas = dict()
+    for letra in texto:
+        if letra not in ja_foi:
+            ja_foi.append(letra)
+            contadas[letra] = texto.count(letra)
+    contadas.pop('\n')
+    messagebox.showinfo(title='Strings Contadas', message=contadas)
+
+
+def segura_ai():
+    mensagem = texto.get('1.0', END)
+    conta_string(mensagem)
+
+
+janela = Tk()
+janela.geometry('250x250')
+janela.configure(background='#dde')
+janela.title('Quantas Letras')
+
+mensagem_inicial = Label(janela, text='DIGITE O TEXTO', background='#dde', foreground='#009', anchor='w')
+mensagem_inicial.place(x=60, y=10, width=100, height=20)
+
+texto = Text(janela)
+texto.place(x=10, y=30, width=200, height=100)
+
+
+botao = Button(janela, text='Contar agora', command=segura_ai)
+botao.place(x=35, y=145, width=150, height=50)
+
+
+janela.mainloop()
